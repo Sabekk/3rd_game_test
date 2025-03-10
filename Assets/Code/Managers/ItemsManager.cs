5b05c9581a957e520a4281387d0b65d88fa51f52
Sebastian Blaszczyk <skb3@op.pl>
2025-03-10 21:04:20 +0100

    Fixed cancel token until taking items

--- Assets/Code/Managers/ItemsManager.cs
+++ Assets/Code/Managers/ItemsManager.cs
@@ -1,6 +1,7 @@
 using Gameplay.Character;
 using Newtonsoft.Json.Linq;
 using Sirenix.OdinInspector;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -59,27 +60,44 @@ namespace Gameplay.Items
         public async Task<List<Item>> GetRandomItems()
         {
             List<Item> items = new();
-            TokenSource = new();
-            string itemsData = await GameServerMock.GetItemsAsync(TokenSource.Token);
 
-            if (TokenSource.IsCancellationRequested == true)
-                return items;
+            if (TokenSource == null || TokenSource.IsCancellationRequested)
+                TokenSource = new CancellationTokenSource();
 
-            JObject jObjectOfItems = JObject.Parse(itemsData);
-            if (jObjectOfItems.TryGetValue("Items", out var jToken))
+            try
             {
-                var children = jToken.Children<JObject>();
-                foreach (var child in children)
+                string itemsData = await GameServerMock.GetItemsAsync(TokenSource.Token);
+
+                if (TokenSource.IsCancellationRequested == true)
+                    return items;
+
+                JObject jObjectOfItems = JObject.Parse(itemsData);
+                if (jObjectOfItems.TryGetValue("Items", out var jToken))
                 {
-                    Item item = new(child);
-                    if (item.Data == null)
+                    var children = jToken.Children<JObject>();
+                    foreach (var child in children)
                     {
-                        Debug.LogError($"Missing data in database for {item.ItemName}, check settings of database");
-                        continue;
+                        Item item = new(child);
+                        if (item.Data == null)
+                        {
+                            Debug.LogError($"Missing data in database for {item.ItemName}, check settings of database");
+                            continue;
+                        }
+                        items.Add(item);
                     }
-                    items.Add(item);
                 }
             }
+            catch (TaskCanceledException) 
+            {
+                Debug.LogWarning("Task of taking items canceled");
+                return items;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Unexpected error: {e}");
+                return items;
+            }
+
 
             return items;
         }
